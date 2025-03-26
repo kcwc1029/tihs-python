3cf2cdf74990adaa00ca28939400dc97e8fe01e5
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-03-26 17:41:39 +0800

    2025.03.25

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
-def cute(a, b):
-    c = a + b
-    for i in range(c):
-        print("拉姆好可愛")
+##### 函數：輸出 #####
+def while_print(num):
+    i = 0
+    while i<num:
+        print(f"{i} 拉姆好可愛")
+        i+=1
     print()
 
+##### 相加 #####
+def cute(a, b):
+    c = a + b
+    return c
+    
 ##### 主程式 #####
-cute(1, 0)
+temp = cute(1, 1)
+ans = cute(temp, temp)
+while_print(ans)
 
-cute(a=1, b=2)
+# 0 拉姆好可愛
+# 1 拉姆好可愛
+# 2 拉姆好可愛
+# 3 拉姆好可愛
