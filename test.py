f24f5dd3856ef8e367c6a200589d593719a7f595
CHEN WEI-CHENG <3331363@gmail.com>
2025-04-06 05:51:36 +0800

    2025.03.03

--- test.py
+++ test.py
@@ -1,28 +1,10 @@
-import tkinter
+arr=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
 
-# 建立視窗物件
-root = tkinter.Tk()
-root.title("顯示迷宮")
-
-# 建立畫布
-canvas = tkinter.Canvas(width=800, height=560, bg="white")
-canvas.pack()
-
-maze = [
-    [1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,1,0,0,1],
-    [1,0,1,1,0,0,1,0,0,1],
-    [1,0,0,1,0,0,0,0,0,1],
-    [1,0,0,1,1,1,1,1,0,1],
-    [1,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1]
-    ]
-
-# 使用雙層迴圈走訪迷宮資料
-for y in range(7):
-    for x in range(10):
-        if maze[y][x] == 1:
-            # 畫出灰色方塊，位置為 (x*80, y*80)，每格是 80x80 大小
-            canvas.create_rectangle(x*80, y*80, x*80+80, y*80+80, fill="gray")
-
-root.mainloop()
+print(arr[0][0])
+print(arr[0][-1])
+print(arr[-1][0])
+print(arr[0][:])
