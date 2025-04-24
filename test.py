ea2b2193d281e79ce11d96393acbaf9f6201ea32
Wei-Cheng Chen <3331363@gmail.com>
2025-04-25 01:09:19 +0800

    2025.04.23

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-question = [
-    "天天跟你黏一起，天冷時才想起。（猜一衣物）：",
-    "什麼東西越洗越髒：",
-    "哪一條線永遠不會有結：",
-    "你的右邊臉頰長得像什麼？：",
-    "哪尊神明不喜歡吵鬧："	
-]
+message = input("臭直男感情判斷器\n請輸入對方訊息：")
 
-ans = ["圍巾", "水", "時間線", "像你的左邊臉頰", "觀世音"]
-
-for i in range(len(question)):
-    print(question[i])
-    n = input()
-    if n == ans[i]:
-        print("答對了")
-    else:
-        print("答錯了")
+if message == "我們可以談談嗎？" or message == "你最近變了" or message == "我覺得我們不適合":
+    print("⚠️ 警告：有分手風險！")
+else:
+    print("💗 放心啦，感情穩定")
